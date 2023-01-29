babc16dec9a24f7c83d178a3228fcebd45e2b573
joaomps <joaomps18@gmail.com>
2023-01-29 16:33:25 +0000

    rename tablename

--- src/index.ts
+++ src/index.ts
@@ -67,7 +67,7 @@ app.delete("/accounts/", async (req, res) => {
 // get all commands
 app.get("/commands", async (req, res) => {
   console.log("Retrieving all commands");
-  const commands = await prisma.commands.findMany({
+  const commands = await prisma.executions.findMany({
     orderBy: { createdat: "desc" },
   });
   if (commands) {
@@ -99,7 +99,7 @@ app.post("/commands/", async (req, res) => {
   // return res.json({})
 
   if (commandstring) {
-    const command = await prisma.commands.create({
+    const command = await prisma.executions.create({
       data: {
         createdat: new Date(),
         execute: commandstring
